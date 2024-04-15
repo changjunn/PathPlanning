1cfbfb870e912830d940ad88c70c33693ca4d48b
이창준 <1631178@mobis.co.kr>
2024-04-15 14:05:24 +0900

    add checkFeasibility

--- calDualCirclePath.py
+++ calDualCirclePath.py
@@ -1,31 +1,83 @@
 import numpy as np
-from math import sin, cos, atan2
-
+from navpy import wrapToPi
+from math import pi, sin, cos, atan2, sqrt, pow, dist
 
 
 def transformGlobalToLocal(originPose, globalPose):
     
-    localPose = np.array()
+    if np.size(globalPose) == 3:
+        localPose = np.array([
+             cos(originPose[2]) * (globalPose[0] - originPose[0]) + sin(originPose[2]) * (globalPose[1] - originPose[1]),
+            -sin(originPose[2]) * (globalPose[0] - originPose[0]) + cos(originPose[2]) * (globalPose[1] - originPose[1]),
+            wrapToPi(globalPose[2] - originPose[2])
+        ])
+        
+    elif np.size(globalPose) == 2:
+        localPose = np.array([
+             cos(originPose[2]) * (globalPose[0] - originPose[0]) + sin(originPose[2]) * (globalPose[1] - originPose[1]),
+            -sin(originPose[2]) * (globalPose[0] - originPose[0]) + cos(originPose[2]) * (globalPose[1] - originPose[1])
+        ])
+        
+    else:
+        localPose = 0
     
     return localPose
 
 
-
 def transformLocalToGlobal(originPose, localPose):
     
-    globalPose = np.array()
-    
+    if np.size(localPose) == 3:
+        globalPose = np.array([
+            cos(originPose[2]) * localPose[0] - sin(originPose[2]) * localPose[1] + originPose[0],
+            sin(originPose[2]) * localPose[0] + cos(originPose[2]) * localPose[1] + originPose[1],
+            originPose[2] + localPose[2]
+        ])
+        
+    elif np.size(localPose) == 2:
+        globalPose = np.array([
+            cos(originPose[2]) * localPose[0] - sin(originPose[2]) * localPose[1] + originPose[0],
+            sin(originPose[2]) * localPose[0] + cos(originPose[2]) * localPose[1] + originPose[1]
+        ])
+        
+    else:
+        globalPose = 0
+            
     return globalPose
 
 
-
-
-def checkFeasibility():
+def checkFeasibility(localPose, minTurningRadius):
+        
+    sectorCenterPnt = np.array([
+        minTurningRadius * abs(sin(localPose[2])),
+        minTurningRadius * np.sign(localPose[2]) * (1 - cos(localPose[2]))
+    ])
     
-    feasibilityFlg = 0
+    sideCircle1Pnt = np.array([
+        sectorCenterPnt[0] - 2 * minTurningRadius * abs(sin(localPose[2])),
+        sectorCenterPnt[1] + 2 * minTurningRadius * np.sign(localPose[2]) * cos(localPose[2])
+    ])
     
-    return feasibilityFlg
-
+    sideCircle2Pnt = np.array([
+        sectorCenterPnt[0],
+        sectorCenterPnt[1] - 2 * minTurningRadius * np.sign(localPose[2])
+    ])
+    
+    dist1 = dist(np.array([sideCircle1Pnt[0], localPose[0]]), np.array([sideCircle1Pnt[1], localPose[1]]))
+    
+    dist2 = dist(np.array([sideCircle2Pnt[0], localPose[0]]), np.array([sideCircle2Pnt[1], localPose[1]]))
+    
+    dist3 = dist(np.array([sectorCenterPnt[0], localPose[0]]), np.array([sectorCenterPnt[1], localPose[1]]))
+    
+    localX = cos(localPose[2] / 2) * (localPose[0] - sectorCenterPnt[0]) + sin(localPose[2] / 2) * (localPose[1] - sectorCenterPnt[1])
+    
+    print(sectorCenterPnt)
+    print(sideCircle1Pnt)
+    print(sideCircle2Pnt)
+    print(dist1)
+    print(dist2)
+    print(dist3)
+    
+    return dist1 >= 2 * minTurningRadius and dist2 >= 2 * minTurningRadius and dist3 <= 2 * minTurningRadius and localX >= 0
 
 
 def calKappa():
@@ -60,11 +112,13 @@ def calWaypoint():
 
 
 
-def calDualCirclePath(initPose, endPose, minTurningRadius):
+def calDualCirclePath(initPose, endPose, minTurningRadius, intervalDist):
     
     localPose = transformGlobalToLocal(initPose, endPose)
         
-    if ~checkFeasibility(localPose, minTurningRadius):
+    print(checkFeasibility(localPose, minTurningRadius))
+        
+    if not checkFeasibility(localPose, minTurningRadius):
         return
     
     calKappa()
@@ -83,8 +137,8 @@ def calDualCirclePath(initPose, endPose, minTurningRadius):
 def main():
     print("calDualCirclePath")
     
-    initPose = np.array(0, 0, np.deg2rad(0))
-    endPose = np.array(5, 1, np.deg2rad(0))
+    initPose = np.array([0, 0, np.deg2rad(0.1)])
+    endPose = np.array([5, 1, np.deg2rad(0)])
     minTurningRadius = 5
     intervalDist = 0.2
     
